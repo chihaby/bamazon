1edfd5d6cffaa0eb9d7f7fcabbc639c97645b8c5
Radouane <38984550+Radouane@users.noreply.github.com>
2019-03-06 09:50:29 -0800

    adds query

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,10 +16,40 @@ connection.connect(function(err) {
 });
 
 function listAllData(){
-    connection.query("SELECT * FROM products", function(err, res) {
+    const query = connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
+        console.log(res[0].stock_quantity);
+        start();
+       // connection.end();
     });
     console.log(query.sql);
+}
+
+function start(){
+    inquirer
+        .prompt([
+            {
+                name: "item_id",
+                type: "input",
+                message: "Enter the product ID, you would like to purchase"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units would you like to buy?"
+            }])
+            .then(function(answer){
+                const query = connection.query("SELECT * FROM products", function(err, res) {
+                    if (err) throw err;
+                    // console.log(res[1].stock_quantity);
+                    var arr = [];
+                for (var i=0; i<res.length; i++){
+                    if (res[i].item_id == answer.item_id){
+                        arr.push(res[i]);
+                        console.log(arr);
+                    }
+                }
+                connection.end();
+            })
+    })
 }
