971fbb81952ad48f3a6449b8bf5e85f755ef2001
Rad <chihaby@gmail.com>
2019-03-09 08:53:46 -0800

    adds inventory feature

--- bamazonManager.js
+++ bamazonManager.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     menu();
@@ -18,7 +18,7 @@ connection.connect(function(err) {
 
 
 //Menu selection
-function menu (){
+function menu() {
     inquirer
         .prompt({
             name: "menu",
@@ -26,24 +26,63 @@ function menu (){
             message: "Select from the following: ",
             choices: ["VIEW PRODUCTS", "VIEW LOW INVENTORY", "ADD TO INVENTORY", "ADD NEW PRODUCT", "EXIT"]
         })
-        .then(function(answer) {
+        .then(function (answer) {
 
             if (answer.menu === "VIEW PRODUCTS") {
-                viewProducts();
+                product();
             }
-            else if(answer.menu === "VIEW LOW INVENTORY") {
-                viewLowInventory();
+            else if (answer.menu === "VIEW LOW INVENTORY") {
+                lowInventory();
             }
-            else if(answer.menu === "ADD TO INVENTORY") {
-                addToInventory();
+            else if (answer.menu === "ADD TO INVENTORY") {
+                addInventory();
             }
-            else if(answer.menu === "ADD NEW PRODUCT") {
-                addNewProduct();
-            }else{
-            connection.end();
+            else if (answer.menu === "ADD NEW PRODUCT") {
+                addProduct();
+            } else {
+                connection.end();
             }
         });
 }
 
+function product() {
+    var query = connection.query("SELECT * FROM products", function (err, response1) {
+        if (err) throw err;
+        console.log(response1);
+    })
+    console.log(query.sql);
+}
 
+function lowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity <= 5", function (err, response2) {
+        if (err) throw err;
+        console.log(response2);
+    })
+    connection.end();
+}
 
+function addInventory() {
+
+    inquirer.prompt([
+        {
+            name: 'item_id',
+            type: "input",
+            message: "Which item would you like to add to Inventory?"
+        },
+        {
+            name: 'quantity',
+            type: "input",
+            message: "How many items are you adding?"
+        }
+    ]).then(function (answer) {
+        connection.query(`UPDATE products SET stock_quantity WHERE item_id = "${answer.item_id}"`, function (err, response3) {
+            if (err) throw err;
+
+            for (var i = 0; i < response3.length; i++) {
+                console.log("=================" + answer.quantity + "New items added");
+                console.log("================= New Inventory " + response3[i].stock_quantity + answer.quantity);
+            }
+        })
+        //connection.end();
+    })
+}
